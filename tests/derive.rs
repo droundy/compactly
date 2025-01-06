4416fe044959fbef265a919b3e1fb0aaa167afd3
David Roundy <daveroundy@gmail.com>
2025-01-05 16:03:00 -0800

    test a few more tupe structs

--- tests/derive.rs
+++ tests/derive.rs
@@ -15,9 +15,30 @@ pub(crate) use assert_size;
 #[test]
 fn singlet_tuple() {
     #[derive(Debug, PartialEq, Eq, compactly::Encode)]
-    pub struct SingletTuple(usize);
+    pub struct Tuple(usize);
 
-    assert_size!(SingletTuple(0), 1);
-    assert_size!(SingletTuple(1), 1);
-    assert_size!(SingletTuple(2), 1);
+    assert_size!(Tuple(0), 1);
+    assert_size!(Tuple(1), 1);
+    assert_size!(Tuple(2), 1);
+}
+
+#[test]
+fn pair_tuple() {
+    #[derive(Debug, PartialEq, Eq, compactly::Encode)]
+    pub struct Tuple(usize, bool);
+
+    assert_size!(Tuple(0, false), 1);
+    assert_size!(Tuple(1, true), 1);
+    assert_size!(Tuple(2, false), 1);
+    assert_size!(Tuple(2048, false), 4);
+}
+
+#[test]
+fn zero_size() {
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, compactly::Encode)]
+    pub struct Tuple;
+
+    assert_size!(Tuple, 1);
+    assert_size!([Tuple; 4], 2);
+    assert_size!([Tuple; 1024], 3);
 }
