5e0d0db468028d2373d96eb7e3dae7cf4d210314
David Roundy <daveroundy@gmail.com>
2025-04-19 07:05:16 -0700

    fix another test

--- src/v1/bools.rs
+++ src/v1/bools.rs
@@ -57,10 +57,10 @@ fn size() {
     use super::assert_bits;
     assert_bits!(true, 1);
     assert_bits!(false, 1);
-    assert_bits!([false; 128], 5);
+    assert_bits!([false; 128], 7);
     assert_bits!([true; 2], 1);
-    assert_bits!([true; 3], 2);
-    assert_bits!([true; 16], 4);
+    assert_bits!([true; 3], 1);
+    assert_bits!([true; 16], 3);
     assert_bits!([true; 64], 5);
     assert_bits!([false, true], 3);
 }
