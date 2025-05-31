574d8982c023bbb091dd157545310758c379c1a6
David Roundy <daveroundy@gmail.com>
2025-05-31 06:45:28 -0700

    simplify option implementation

--- src/v0/option.rs
+++ src/v0/option.rs
@@ -1,10 +1,11 @@
 use super::{Encode, EncodingStrategy};
+use crate::{Compressible, Normal, Small};
 
-pub struct OptionContext<T: Encode> {
+pub struct OptionContext<T, S: EncodingStrategy<T>> {
     is_some: <bool as Encode>::Context,
-    value: T::Context,
+    value: S::Context,
 }
-impl<T: Encode> Default for OptionContext<T> {
+impl<T, S: EncodingStrategy<T>> Default for OptionContext<T, S> {
     #[inline]
     fn default() -> Self {
         Self {
@@ -14,7 +15,7 @@ impl<T: Encode> Default for OptionContext<T> {
     }
 }
 impl<T: Encode> Encode for Option<T> {
-    type Context = OptionContext<T>;
+    type Context = OptionContext<T, Normal>;
     #[inline]
     fn encode<W: std::io::Write>(
         &self,
@@ -42,45 +43,35 @@ impl<T: Encode> Encode for Option<T> {
 }
 
 macro_rules! option_encoding_strategy {
-    ($t:ty, $strategy:ident, $mod:ident) => {
-        mod $mod {
-            use super::{Encode, EncodingStrategy};
-            use crate::$strategy;
-            #[derive(Default)]
-            pub struct Context {
-                is_some: <bool as Encode>::Context,
-                value: <$strategy as EncodingStrategy<$t>>::Context,
-            }
-
-            impl EncodingStrategy<Option<$t>> for $strategy {
-                type Context = Context;
-                fn encode<W: std::io::Write>(
-                    value: &Option<$t>,
-                    writer: &mut super::super::Writer<W>,
-                    ctx: &mut Self::Context,
-                ) -> Result<(), std::io::Error> {
-                    if let Some(v) = value {
-                        true.encode(writer, &mut ctx.is_some)?;
-                        $strategy::encode(v, writer, &mut ctx.value)
-                    } else {
-                        false.encode(writer, &mut ctx.is_some)
-                    }
+    ($t:ty, $strategy:ident) => {
+        impl EncodingStrategy<Option<$t>> for $strategy {
+            type Context = OptionContext<$t, $strategy>;
+            fn encode<W: std::io::Write>(
+                value: &Option<$t>,
+                writer: &mut super::Writer<W>,
+                ctx: &mut Self::Context,
+            ) -> Result<(), std::io::Error> {
+                if let Some(v) = value {
+                    true.encode(writer, &mut ctx.is_some)?;
+                    $strategy::encode(v, writer, &mut ctx.value)
+                } else {
+                    false.encode(writer, &mut ctx.is_some)
                 }
-                fn decode<R: std::io::Read>(
-                    reader: &mut super::super::Reader<R>,
-                    ctx: &mut Self::Context,
-                ) -> Result<Option<$t>, std::io::Error> {
-                    if bool::decode(reader, &mut ctx.is_some)? {
-                        Ok(Some($strategy::decode(reader, &mut ctx.value)?))
-                    } else {
-                        Ok(None)
-                    }
+            }
+            fn decode<R: std::io::Read>(
+                reader: &mut super::Reader<R>,
+                ctx: &mut Self::Context,
+            ) -> Result<Option<$t>, std::io::Error> {
+                if bool::decode(reader, &mut ctx.is_some)? {
+                    Ok(Some($strategy::decode(reader, &mut ctx.value)?))
+                } else {
+                    Ok(None)
                 }
             }
         }
     };
 }
 
-option_encoding_strategy!(String, Compressible, small_string);
-option_encoding_strategy!(u64, Small, small_u64);
-option_encoding_strategy!(usize, Small, small_usize);
+option_encoding_strategy!(String, Compressible);
+option_encoding_strategy!(u64, Small);
+option_encoding_strategy!(usize, Small);
--- src/v1/option.rs
+++ src/v1/option.rs
@@ -1,10 +1,11 @@
 use super::{Encode, EncodingStrategy, LowCardinality};
+use crate::{Compressible, Normal, Small};
 
-pub struct OptionContext<T: Encode> {
+pub struct OptionContext<T, S: EncodingStrategy<T>> {
     is_some: <bool as Encode>::Context,
-    value: T::Context,
+    value: S::Context,
 }
-impl<T: Encode> Default for OptionContext<T> {
+impl<T, S: EncodingStrategy<T>> Default for OptionContext<T, S> {
     #[inline]
     fn default() -> Self {
         Self {
@@ -13,7 +14,7 @@ impl<T: Encode> Default for OptionContext<T> {
         }
     }
 }
-impl<T: Encode> Clone for OptionContext<T> {
+impl<T, S: EncodingStrategy<T>> Clone for OptionContext<T, S> {
     fn clone(&self) -> Self {
         Self {
             is_some: self.is_some.clone(),
@@ -22,7 +23,7 @@ impl<T: Encode> Clone for OptionContext<T> {
     }
 }
 impl<T: Encode> Encode for Option<T> {
-    type Context = OptionContext<T>;
+    type Context = OptionContext<T, Normal>;
     #[inline]
     fn encode<W: std::io::Write>(
         &self,
@@ -34,6 +35,14 @@ impl<T: Encode> Encode for Option<T> {
             v.encode(writer, &mut ctx.value)
         } else {
             false.encode(writer, &mut ctx.is_some)
+        }
+    }
+    #[inline]
+    fn millibits(&self, ctx: &mut Self::Context) -> Option<usize> {
+        if let Some(v) = self {
+            Some(true.millibits(&mut ctx.is_some)? + v.millibits(&mut ctx.value)?)
+        } else {
+            false.millibits(&mut ctx.is_some)
         }
     }
     #[inline]
@@ -50,58 +59,48 @@ impl<T: Encode> Encode for Option<T> {
 }
 
 macro_rules! option_encoding_strategy {
-    ($t:ty, $strategy:ident, $mod:ident) => {
-        mod $mod {
-            use super::{Encode, EncodingStrategy};
-            use crate::$strategy;
-            #[derive(Default, Clone)]
-            pub struct Context {
-                is_some: <bool as Encode>::Context,
-                value: <$strategy as EncodingStrategy<$t>>::Context,
+    ($t:ty, $strategy:ident) => {
+        impl EncodingStrategy<Option<$t>> for $strategy {
+            type Context = OptionContext<$t, $strategy>;
+            fn encode<W: std::io::Write>(
+                value: &Option<$t>,
+                writer: &mut crate::v1::Writer<W>,
+                ctx: &mut Self::Context,
+            ) -> Result<(), std::io::Error> {
+                if let Some(v) = value {
+                    true.encode(writer, &mut ctx.is_some)?;
+                    $strategy::encode(v, writer, &mut ctx.value)
+                } else {
+                    false.encode(writer, &mut ctx.is_some)
+                }
             }
-
-            impl EncodingStrategy<Option<$t>> for $strategy {
-                type Context = Context;
-                fn encode<W: std::io::Write>(
-                    value: &Option<$t>,
-                    writer: &mut crate::v1::Writer<W>,
-                    ctx: &mut Self::Context,
-                ) -> Result<(), std::io::Error> {
-                    if let Some(v) = value {
-                        true.encode(writer, &mut ctx.is_some)?;
-                        $strategy::encode(v, writer, &mut ctx.value)
-                    } else {
-                        false.encode(writer, &mut ctx.is_some)
-                    }
+            fn millibits(value: &Option<$t>, ctx: &mut Self::Context) -> Option<usize> {
+                if let Some(v) = value {
+                    Some(
+                        true.millibits(&mut ctx.is_some)?
+                            + $strategy::millibits(v, &mut ctx.value)?,
+                    )
+                } else {
+                    false.millibits(&mut ctx.is_some)
                 }
-                fn millibits(value: &Option<$t>, ctx: &mut Self::Context) -> Option<usize> {
-                    if let Some(v) = value {
-                        Some(
-                            true.millibits(&mut ctx.is_some)?
-                                + $strategy::millibits(v, &mut ctx.value)?,
-                        )
-                    } else {
-                        false.millibits(&mut ctx.is_some)
-                    }
-                }
-                fn decode<R: std::io::Read>(
-                    reader: &mut crate::v1::Reader<R>,
-                    ctx: &mut Self::Context,
-                ) -> Result<Option<$t>, std::io::Error> {
-                    if bool::decode(reader, &mut ctx.is_some)? {
-                        Ok(Some($strategy::decode(reader, &mut ctx.value)?))
-                    } else {
-                        Ok(None)
-                    }
+            }
+            fn decode<R: std::io::Read>(
+                reader: &mut crate::v1::Reader<R>,
+                ctx: &mut Self::Context,
+            ) -> Result<Option<$t>, std::io::Error> {
+                if bool::decode(reader, &mut ctx.is_some)? {
+                    Ok(Some($strategy::decode(reader, &mut ctx.value)?))
+                } else {
+                    Ok(None)
                 }
             }
         }
     };
 }
 
-option_encoding_strategy!(String, Compressible, compressible_string);
-option_encoding_strategy!(u64, Small, small_u64);
-option_encoding_strategy!(usize, Small, small_usize);
+option_encoding_strategy!(String, Compressible);
+option_encoding_strategy!(u64, Small);
+option_encoding_strategy!(usize, Small);
 
 #[derive(Default, Clone)]
 pub struct LowContext<C: Default> {
