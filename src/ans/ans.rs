5c46418521a39b7482dba0abc32714f64f9178ea
David Roundy <daveroundy@gmail.com>
2025-07-05 05:41:54 -0700

    first reorg of ans Coder

--- src/ans/ans.rs
+++ src/ans/ans.rs
@@ -1,31 +1,55 @@
 mod probability;
 pub use probability::Probability;
+mod bytes;
+use bytes::Bytes;
 
-type State = u32;
+type State = u64;
 
-#[derive(Eq, PartialEq, Debug)]
-pub struct Coder {
-    state: State,
-    bulk: Vec<u8>,
+#[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Writer {
+    bits: Vec<(bool, Probability)>,
 }
 
-impl Coder {
-    pub fn new() -> Self {
-        Self {
-            state: 255,
-            bulk: Vec::new(),
+impl Writer {
+    #[inline]
+    pub fn encode(&mut self, probability_of_false: Probability, value: bool) {
+        self.bits.push((value, probability_of_false));
+    }
+    #[inline]
+    pub fn finish(self) -> Vec<u8> {
+        let mut coder = Encoder::new();
+        let mut out = Vec::new();
+        for (b, probability) in self.bits.into_iter().rev() {
+            if let Some(byte) = coder.encode(b, probability) {
+                out.push(byte);
+            }
         }
+        out.extend(coder.finish_encoding());
+        out.reverse();
+        out
+    }
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct Encoder {
+    state: State,
+}
+
+impl Encoder {
+    pub fn new() -> Self {
+        Self { state: 255 }
     }
 
     /// Encode a bit using distribution Bernoulli(probability).
-    pub fn encode(&mut self, b: bool, probability: Probability) {
+    fn encode(&mut self, b: bool, probability: Probability) -> Option<u8> {
+        let mut out = None;
         let zeros = State::from(probability);
         let ones = 256 - zeros;
         // we use uniform of size matching the bit value to decode from state first
         let freq = if b { zeros } else { ones };
         // shift data from state to bulk when it grows too much
         if self.state >> (State::BITS - 8) >= freq {
-            self.bulk.push(self.state as u8);
+            out = Some(self.state as u8);
             self.state >>= 8;
         }
         // the code really starts here, decode digit from freq base
@@ -35,6 +59,39 @@ impl Coder {
         }
         // now encode new digit from 256 base
         self.state = (self.state / freq) * 256 + z;
+        out
+    }
+
+    pub fn finish_encoding(&mut self) -> Bytes {
+        let mut bytes = Bytes::default();
+        while self.state > 0 {
+            bytes.push(self.state as u8);
+            self.state >>= 8;
+        }
+        bytes
+    }
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct Decoder<'a> {
+    state: State,
+    bytes: &'a [u8],
+}
+
+impl<'a> Decoder<'a> {
+    pub fn new(bytes: &'a [u8]) -> Self {
+        let mut state: State = 0;
+        if bytes.len() < 8 {
+            for &b in bytes {
+                state = (state << 8) | State::from(b);
+            }
+
+            Self { state, bytes: &[] }
+        } else {
+            let state = State::from_be_bytes(bytes[0..8].try_into().unwrap());
+            let bytes = &bytes[8..];
+            Self { state, bytes }
+        }
     }
 
     /// Decode a bit using distribution Bernoulli(probability).
@@ -51,8 +108,8 @@ impl Coder {
             self.state = self.state * ones + z;
         }
         if self.state < 1 << (State::BITS - 8) {
-            if let Some(u) = self.bulk.pop() {
-                self.state = (self.state << 8) | u as State;
+            if let Some(&u) = self.bytes.split_off_first() {
+                self.state = (self.state << 8) | State::from(u);
             }
         }
         b
@@ -66,13 +123,16 @@ fn check_ans_coder() {
     data.resize_with(SIZE, || rand::random::<bool>());
     let mut distros = Vec::new();
     distros.resize_with(SIZE, rand::random::<Probability>);
-    let mut coder = Coder::new();
-    for (b, probability) in data.iter().copied().zip(distros.iter().copied()).rev() {
-        // rev here
-        coder.encode(b, probability);
-    }
+    let mut writer = Writer::default();
     for (b, probability) in data.iter().copied().zip(distros.iter().copied()) {
-        assert_eq!(coder.decode(probability), b);
+        // rev here
+        writer.encode(probability, b);
     }
-    assert_eq!(coder, Coder::new());
+    let bytes = writer.finish();
+    let mut decoder = Decoder::new(&bytes);
+    for (b, probability) in data.iter().copied().zip(distros.iter().copied()) {
+        println!("checking {b} {probability}");
+        assert_eq!(decoder.decode(probability), b);
+    }
+    assert_eq!(decoder.state, 255);
 }
