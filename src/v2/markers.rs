3f538ad878584ab87cdcc1942794194d8f6f914b
Miner3DGaming <105589865+Miner3DGaming@users.noreply.github.com>
2026-05-10 02:07:22 +0200

    Added compatibility for PhantomData and PhantomPinned for Encoding V2

---
+++ src/v2/markers.rs
@@ -0,0 +1,47 @@
+use std::marker::{PhantomData, PhantomPinned};
+
+use super::Encode;
+
+impl<T> Encode for PhantomData<T> {
+    type Context = ();
+
+    #[inline]
+    fn encode<E: super::EntropyCoder>(
+        &self,
+        _encoder: &mut E,
+        _ctx: &mut Self::Context,
+    ) {
+        // PhantomData carries no runtime information, so encoding is a no-op
+    }
+
+    #[inline]
+    fn decode<D: super::EntropyDecoder>(
+        _decoder: &mut D,
+        _ctx: &mut Self::Context,
+    ) -> Result<Self, std::io::Error> {
+        // PhantomData can always be constructed without decoding anything
+        Ok(PhantomData)
+    }
+}
+
+impl Encode for PhantomPinned {
+    type Context = ();
+
+    #[inline]
+    fn encode<E: super::EntropyCoder>(
+        &self,
+        _encoder: &mut E,
+        _ctx: &mut Self::Context,
+    ) {
+        // PhantomData carries no runtime information, so encoding is a no-op
+    }
+
+    #[inline]
+    fn decode<D: super::EntropyDecoder>(
+        _decoder: &mut D,
+        _ctx: &mut Self::Context,
+    ) -> Result<Self, std::io::Error> {
+        // PhantomData can always be constructed without decoding anything
+        Ok(PhantomPinned)
+    }
+}
